f838385ed22e93585cbbaabafa0eb586d9d17071
naoya-sugiura <rbs1010@icloud.com>
2020-05-04 12:29:41 +0900

    update

--- scripts/make_testcase.sh
+++ scripts/make_testcase.sh
@@ -13,13 +13,18 @@ fi
 
 cd $PWD/..
 TEST_CASE_DIR="test_case/${1%.*}"
+if ! [ -e $TEST_CASE_DIR ]; then
+    mkdir $TEST_CASE_DIR
+    echo "setup test case dir"
+fi
+
 TEST_CASE_IN=0
 for pathfile in $TEST_CASE_DIR/*.in; do
     CURR_TEST_CASE=${pathfile##*/}
     CURR_TEST_CASE=${CURR_TEST_CASE%.*}
-        if [ $TEST_CASE_IN -lt $CURR_TEST_CASE ]; then
-            TEST_CASE_IN=$CURR_TEST_CASE
-        fi
+    if [ $TEST_CASE_IN -lt $CURR_TEST_CASE ]; then
+        TEST_CASE_IN=$CURR_TEST_CASE
+    fi
 done
 TEST_CASE_OUT=${TEST_CASE_DIR}/$(($TEST_CASE_IN+1)).out
 TEST_CASE_IN=${TEST_CASE_DIR}/$(($TEST_CASE_IN+1)).in
@@ -31,5 +36,11 @@ if ! [ -e $BIN_FILE ]; then
 fi
 
 ARG_IN=${@#* }
+echo "===== input ====="
+echo "$ARG_IN"
 echo "$ARG_IN" >> $TEST_CASE_IN
 ./$BIN_FILE < $TEST_CASE_IN >> $TEST_CASE_OUT
+echo "===== output ====="
+./$BIN_FILE < $TEST_CASE_IN
+echo ""
+echo "generated \"$TEST_CASE_IN\" and \"$TEST_CASE_OUT\""
